12d040bbda5bfae5933f8c08820198c3b5308f3d
Kieran Wake <kieranwake1234@gmail.com>
2024-02-12 12:46:52 +0000

    implemented modus operandi, ready to proceed with solution research

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function countTrue(arr) {
-    //
-	
+    //We need to return the number of 'trues' that appear in a given array.
+    //0 if given array is empty
+    //all array items are of the type bool, true or false.
+    //need to convert answer into an integer. 
+
+    //SOLUTION: 
+
+    //for loop to interate over the array? 
+    //increment it by 1.  
+    //conditional statment to determine returned value of 'trues' 
+    //look into the filter and reduce methods. 
+    //bring it together. 
 }
